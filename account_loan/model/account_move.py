2c21518f374753262ed3cee28fe8703fadacd9b6
Ross Savage <ross.savage@agrista.com>
2020-06-23 07:44:43 +0200

    [FIX] account_loan: Correct long term account lines duplication

--- account_loan/model/account_move.py
+++ account_loan/model/account_move.py
@@ -19,30 +19,6 @@ class AccountMove(models.Model):
         ondelete='restrict',
     )
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if vals.get('loan_line_id'):
-                loan_line_id = self.env['account.loan.line'].browse(vals.get('loan_line_id'))
-                if (loan_line_id and loan_line_id.long_term_loan_account_id
-                        and loan_line_id.long_term_principal_amount != 0):
-                    line_ids = vals.get('line_ids', [])
-                    line_ids.append((0, 0, {
-                        'account_id':
-                        loan_line_id.loan_id.short_term_loan_account_id.id,
-                        'credit':
-                        loan_line_id.long_term_principal_amount,
-                        'debit':
-                        0,
-                    }))
-                    line_ids.append((0, 0, {
-                        'account_id': loan_line_id.long_term_loan_account_id.id,
-                        'credit': 0,
-                        'debit': loan_line_id.long_term_principal_amount,
-                    }))
-                    vals['line_ids'] = line_ids
-        return super().create(vals_list)
-
     def post(self):
         res = super().post()
         for record in self:
